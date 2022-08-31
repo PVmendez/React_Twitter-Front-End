b24e47ea771772fc3ce40843caced7ced64cb4bd
Guillermo <guillermogalain@gmail.com>
2022-08-31 15:18:09 -0300

    changes2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import Tweet from "./pages/Tweet";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>React twitter</h1>
-    </div>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<Landing />} />
+			<Route path="/home" element={<Home />} />
+			<Route path="/login" element={<Login />} />
+			<Route path="/register" element={<Register />} />
+			<Route path="/:userName" element={<Profile />} />
+			<Route path="/tweet/:tweetId" element={<Tweet />} />
+		</Routes>
+	);
 }
 
 export default App;
